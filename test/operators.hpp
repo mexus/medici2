b5f5bfec250ae971f81983b190ebd8dd614813f9
mexus <gilaldpellaeon@gmail.com>
2014-08-08 11:19:08 +0400

    Added a printing funciton for std::array<T, N>

--- test/operators.hpp
+++ test/operators.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <set>
 #include <ostream>
+#include <array>
 
 template<class T>
 std::ostream& PrintVector(std::ostream& s, const T& vec){
@@ -23,6 +24,11 @@ std::ostream& operator<<(std::ostream& s, const std::vector<T>& vec){
 template<class T>
 std::ostream& operator<<(std::ostream& s, const std::set<T>& vec){
 	return PrintVector(s, vec);
+}
+
+template<class T, std::size_t N>
+std::ostream& operator<<(std::ostream& s, const std::array<T, N>& array){
+	return PrintVector(s, array);
 }
 
 #endif /* TEST_OPERATORS_HPP */
