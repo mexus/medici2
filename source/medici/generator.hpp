fd7756546e4cf2f225a6512f6c1ddfb3eb100b08
mexus <gilaldpellaeon@gmail.com>
2014-08-12 11:48:12 +0400

    Fixed generation algorithm

--- source/medici/generator.hpp
+++ source/medici/generator.hpp
@@ -7,9 +7,9 @@ namespace medici{
 
 	template<std::size_t N, class TestBeforeFunctor, class TestAfterFunctor>
 	void Generator::Generate(std::array<Card, N>& deck, Patience::PatienceInfo& info, Mixer<Card, N>& mixer, const TestBeforeFunctor& beforeFunctor, const TestAfterFunctor& afterFunctor){
-		while (!(beforeFunctor(deck) && Patience::Converge(deck, info) && afterFunctor(deck, info))){
+		do {
 			mixer.Mix(deck);
-		}
+		} while (!(beforeFunctor(deck) && Patience::Converge(deck, info) && afterFunctor(deck, info)));
 	}
 
 }
