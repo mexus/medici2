0733e4764688ad1d3d8a634e79a330e62852a36e
Mexus <gilaldpellaeon@gmail.com>
2015-03-01 18:52:16 +0300

    Removed extra logging

--- gui/main_form.cpp
+++ gui/main_form.cpp
@@ -33,9 +33,7 @@ void MainForm::RenameSelector(int index) {
 }
 
 MainForm::MainForm(QWidget* parent) : QMainWindow(parent) {
-        S_LOG("MainForm");
         QSettings settings;
-        log(logxx::debug) << "Restoring geometry and state" << logxx::endl;
         restoreGeometry(settings.value("main-window:geometry").toByteArray());
         restoreState(settings.value("main-window:state").toByteArray());
 
@@ -60,9 +58,7 @@ MainForm::~MainForm() {
 }
 
 void MainForm::closeEvent(QCloseEvent*) {
-        S_LOG("closeEvent");
         QSettings settings;
-        log(logxx::debug) << "Saving geometry and state" << logxx::endl;
         settings.setValue("main-window:geometry", saveGeometry());
         settings.setValue("main-window:state", saveState());
 }
