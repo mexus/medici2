e55aa7779e5ae7f46bf7d9a97f7e1c934d3d1df6
Mexus <gilaldpellaeon@gmail.com>
2015-03-01 15:42:16 +0300

    Working on a deck selector

--- gui/deck_selector.cpp
+++ gui/deck_selector.cpp
@@ -1,10 +1,92 @@
 #include "deck_selector.h"
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QPushButton>
+
+GuiDeckSelector::GuiDeckSelector(QWidget *parent) : QWidget(parent) {
+	CreateElements();
+	SetSpinBoxes();
+	CreateLayout();
+}
 
 void GuiDeckSelector::AddCardSelector(int suit, int rank, bool inversed) {
 	auto selector = new GuiCardSelector(suit, rank, inversed);
 	selectors.insert(selector);
 	QObject::connect(selector, &GuiCardSelector::DeleteClicked, [this, selector](){
 			selectors.erase(selector);
+			selector->disconnect();
+			selectorsLayout->removeWidget(selector);
+			selector->deleteLater();
 			});
+	selectorsLayout->insertWidget(selectorsLayout->count() - 1, selector);
+}
+
+QSpinBox* GuiDeckSelector::CreateSpinBox(int min, int max) {
+	auto box = new QSpinBox();
+	box->setRange(min, max);
+	box->setSingleStep(1);
+	return box;
+}
+
+void GuiDeckSelector::CreateElements() {
+	selectorMode = new QComboBox();
+	selectorMode->addItem(tr("At least one of conditions must match"), SELECT_ONE);
+	selectorMode->addItem(tr("All conditions must match"), SELECT_ALL);
+	selectorMode->setCurrentIndex(0);
+}
+
+void GuiDeckSelector::SetSpinBoxes() {
+	positionStart = CreateSpinBox(1, 36);
+	positionEnd = CreateSpinBox(1, 36);
+}
+
+void GuiDeckSelector::AddPushButton(const QString& name, QLayout* layout) {
+}
+
+void GuiDeckSelector::CreateLayout() {
+	auto mainLayout = new QVBoxLayout();
+	mainLayout->addWidget(selectorMode);
+	{
+		auto layout = new QHBoxLayout();
+		layout->addWidget(new QLabel(tr("Position from: ")));
+		layout->addWidget(positionStart);
+		layout->addWidget(new QLabel(tr(" to: ")));
+		layout->addWidget(positionEnd);
+		mainLayout->addItem(layout);
+	}
+	{
+		selectorsLayout = new QHBoxLayout();
+		{
+			auto button = new QPushButton(tr("Add a card selector"));
+			selectorsLayout->addWidget(button);
+			QObject::connect(button, &QPushButton::clicked, this, &GuiDeckSelector::AddCardSelector);
+		}
+		mainLayout->addItem(selectorsLayout);
+	}
+	setLayout(mainLayout);
+}
+
+std::unique_ptr<DeckAbstractSelector> GuiDeckSelector::GetSelector() const {
+	std::unique_ptr<DeckAbstractSelector> selector;
+	SelectorMode mode = static_cast<SelectorMode>(selectorMode->currentData().toUInt());
+
+	std::vector<CardSelector> cardSelectors;
+	for (auto &guiCardSelector: selectors) {
+		cardSelectors.push_back(std::move(guiCardSelector->GetSelector()));
+	}
+	std::size_t from = positionStart->value();
+	std::size_t to = positionEnd->value();
+
+	switch (mode) {
+		case SELECT_ONE:
+			selector.reset(new DeckOneSelector(std::move(cardSelectors), from, to));
+			break;
+		case SELECT_ALL:
+			selector.reset(new DeckAllSelector(std::move(cardSelectors), from, to));
+			break;
+		default:
+			throw std::logic_error("Unknown mode");
+	}
+	return selector;
 }
 
--- gui/deck_selector.h
+++ gui/deck_selector.h
@@ -1,20 +1,42 @@
 #ifndef GUI_DECK_SELECTOR_H
 #define GUI_DECK_SELECTOR_H
 
-#include <cards/deck-selector.h>
 #include <QWidget>
+#include <QComboBox>
+#include <QSpinBox>
+#include <QHBoxLayout>
+
 #include <set>
 
+#include <cards/deck-selector.h>
 #include "card_selector.h"
 
 class GuiDeckSelector : public QWidget {
 Q_OBJECT
 public:
+	GuiDeckSelector(QWidget* parent = 0);
+
 	std::unique_ptr<DeckAbstractSelector> GetSelector() const;
 private:
 	void AddCardSelector(int suit = -1, int rank = -1, bool inversed = false);
 	std::set<GuiCardSelector*> selectors;
+	
+	enum SelectorMode {
+		SELECT_ALL,
+		SELECT_ONE
+	};
 
+	QComboBox* selectorMode;
+	QSpinBox *positionStart, *positionEnd;
+
+	QHBoxLayout* selectorsLayout;
+
+	void CreateElements();
+	virtual void SetSpinBoxes();
+	void CreateLayout();
+
+	static QSpinBox* CreateSpinBox(int min, int max);
+	static void AddPushButton(const QString& name, QLayout* layout);
 };
 
 #endif /* GUI_DECK_SELECTOR_H */
