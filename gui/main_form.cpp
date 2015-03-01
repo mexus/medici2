2d1176e3707e2f1d4c6c5496d349403d33278498
Mexus <gilaldpellaeon@gmail.com>
2015-03-01 17:55:28 +0300

    Added functionality to a main form

--- gui/main_form.cpp
+++ gui/main_form.cpp
@@ -1,5 +1,11 @@
 #include "main_form.h"
+
 #include <QSettings>
+#include <QVBoxLayout>
+#include <QTabWidget>
+#include <QPushButton>
+
+#include "deck_selector.h"
 
 logxx::Log MainForm::cLog("MainForm");
 
@@ -9,6 +15,18 @@ MainForm::MainForm(QWidget* parent) : QMainWindow(parent) {
         log(logxx::debug) << "Restoring geometry and state" << logxx::endl;
         restoreGeometry(settings.value("main-window:geometry").toByteArray());
         restoreState(settings.value("main-window:state").toByteArray());
+
+	setCentralWidget(new QWidget());
+	auto layout = new QVBoxLayout();
+
+	layout->addWidget(new QPushButton(tr("Add conditions set")));
+
+	auto tabs = new QTabWidget();
+	tabs->addTab(new GuiDeckSelector(), tr("Test"));
+	tabs->addTab(new GuiDeckSelector(), tr("Test2"));
+	layout->addWidget(tabs);
+
+	centralWidget()->setLayout(layout);
 }
 
 MainForm::~MainForm() {
