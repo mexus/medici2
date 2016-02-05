2cd04d57acb330ab7b2a420d91ff7c0731075ee5
Mexus <gilaldpellaeon@gmail.com>
2016-02-05 23:16:35 +0300

    Added "ycm-flags" as a submodule

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,7 +1,7 @@
 import sys, os
 
 here = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.normpath(os.path.join(here, '../ycm')))
+sys.path.insert(0, os.path.normpath(os.path.join(here, 'ycm')))
 
 import ycmflags
 
