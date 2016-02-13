04d5d2e1f1b0e8a1fdb7dab1872bba9cb2f038c6
Mexus <gilaldpellaeon@gmail.com>
2016-02-13 16:41:33 +0300

    Removed self-made tests and gui

--- gui/widgets/clickable_group_box.cpp
+++
@@ -1,8 +0,0 @@
-#include "clickable_group_box.h"
-#include <QMouseEvent>
-
-void ClicableGroupBox::mouseDoubleClickEvent(QMouseEvent* e) {
-    if (e->button() == Qt::LeftButton)
-        emit doubleClicked();
-    QGroupBox::mouseDoubleClickEvent(e);
-}
